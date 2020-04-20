3732595841d8437e6532de43c3cf6db34f47faf6
Paul S. Lou <paul96lou@gmail.com>
2020-04-20 01:15:45 -0700

    first draft done, verification works

--- verification.py
+++ verification.py
@@ -1,74 +1,146 @@
+# Covid19 Symptom Survey Crypto
+# Python3
+
 import sys
 import argparse
 import hashlib, base64
 import secrets
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from enum import Enum
 
-# Number of potential salt values
-SALT_SIZE = 10001
-BYTE_LEN_RAND = 16
-RECEIPT_LEN = 16
+
+SALT_SIZE = 10001     # Number of potential salt values
+BYTE_LEN_RAND = 16  # Size of randomness for random prefix to SHA256 input in generating receipt
+RECEIPT_LEN = 16        # Size of receipt
 
 # Receipt types
 class ReceiptType(Enum):
     HOUR = 0
     ZIPCODE = 1
 
+# VerificationTable
+# Keeps track of (value, code) mappings
+# - values are instances of the appropriate receipt type (e.g. the datetime object of the corresponding hour)
+# - codes are hashes of the receipts for the corresponding value
+class VerificationTable:
+    _receiptType = 0
+    _valueToCodeDict = {}
+    _codeToValueDict = {}
+    def __init__(self, receiptType):
+        self._receiptType = receiptType
+    def getType():
+       return self._receiptType
+    def addCodeValuePair(self, code, value):
+       self._valueToCodeDict[value] = code
+       self._codeToValueDict[code] = value
+    def removeCodeValuePair(self, code, value):
+       if (value in self._valueToCodeDict.keys()):
+          del self._valueToValueDict[value]
+       if (code in self._codeToCodeDict.keys()):
+          del self._codeToValueDict[code]
+    def getCode(self, value):
+        if (value in self._valueToCodeDict.keys()):
+            return self._valueToCodeDict[value]
+        return None
+    def getValue(self, code):
+        if (code in self._codeToValueDict.keys()):
+            return self._codeToValueDict[code]
+        return None
 
-def getCurrentHour():
-  now = datetime.now()
-  hourStr = now.strftime("%H")
-  return hourStr
+    # Returns an unsorted list of (value, code) pairs
+    def getCodeValueList(self):
+        lst = []
+        for value in self._valueToCodeDict.keys():
+            lst.append((value, self._valueToCodeDict[value]))
+        return lst
 
+    # If values can be compared, will return a list of (value, code) pairs, sorted by value
+    # Undefined behavior if values are incomparable
+    def getSortedCodeValueList(self):
+        lst = []
+        for value in sorted(self._valueToCodeDict.keys()):
+            lst.append((value, self._valueToCodeDict[value]))
+        return lst
 
-def getCurrentDate():
-  today = date.today()
-  todayStr = today.strftime("%d/%m/%Y")
-  return todayStr
+    # If values can be compared, will return a list of (value, code) pairs with values between valueStart and valueEnd, sorted by value
+    # Undefined behavior if values are incomparable
+    def getSortedCodeValueList(self, valueStart, valueEnd):
+        lst = []
+        for value in sorted(self._valueToCodeDict.keys()):
+            if (valueStart <= value and value <= valueEnd):
+                lst.append((value, self._valueToCodeDict[value]))
+        return lst
+
+    # If values can be compared, will return a VerificationTable that contains only the (value, code) pairs with values between valueStart and valueEnd
+    # Undefined behavior if values are incomparable
+    def getSubTable(self, valueStart, valueEnd):
+        table = VerificationTable(self._receiptType)
+        for value in sorted(self._valueToCodeDict.keys()):
+            if (valueStart <= value and value <= valueEnd):
+                table.addCodeValuePair((value, self._valueToCodeDict[value]))
+        return table
 
 
 def randSaltGenerator():
-  secretsGenerator = secrets.SystemRandom()
-  randSalt = secretsGenerator.randint(0, SALT_SIZE)
-  return randSalt
+    secretsGenerator = secrets.SystemRandom()
+    randSalt = secretsGenerator.randint(0, SALT_SIZE)
+    return randSalt
 
 
 def randBytesGenerator(k):
-  return secrets.token_bytes(k)
+    return secrets.token_bytes(k)
 
 
-def generateReceipt(receiptType, hour, date):
-  r = randBytesGenerator(BYTE_LEN_RAND)
-  sha = hashlib.sha256()
-  if (receiptType == ReceiptType.HOUR):
-      sha.update(b'0')
-      sha.update(r)
-      sha.update(bytes(str.encode(hour)))
-      sha.update(bytes(str.encode(date)))
-  elif (receiptType == ReceiptType.ZIPCODE):
-      sha.update(b'1')
-      sha.update(r)
-      sha.update(bytes(str.encode(hour)))
-      sha.update(bytes(str.encode(date)))
-  code = sha.hexdigest()
-  return code[:RECEIPT_LEN]
-
-# Creates a dictionary of hash codes for the day
-# hashDict[type][hash] = hour of verifying code
-#def generateHashDict(day):
+def generateReceipt(receiptType, value):
+    r = randBytesGenerator(BYTE_LEN_RAND)
+    sha = hashlib.sha256()
+    if (receiptType == ReceiptType.HOUR):
+        sha.update(b'0')
+        sha.update(r)
+        sha.update(bytes(str.encode(value.strftime("%H"))))
+        sha.update(bytes(str.encode(value.strftime("%d/%m/%Y"))))
+    elif (receiptType == ReceiptType.ZIPCODE):
+        sha.update(b'1')
+        sha.update(r)
+        sha.update(bytes(str.encode(value)))
+    code = sha.hexdigest()
+    return code[:RECEIPT_LEN]
 
 
+def allHours(numHoursInFuture, startdate = datetime.now()):
+    hour = timedelta(hours = 1)
+    return [startdate + (hour * k) for k in range(numHoursInFuture)]
 
 
-def verifyCode(receiptType, receipt, hashList):
+# For ReceiptType = HOUR only
+# Returns a list of (datetime, receipt) pairs for each hour over numDays starting from startDate
+def generateDatetimeReceiptPairs(numDays, startdate = datetime.now()):
+    numHours = numDays * 24
+    dateList = allHours(numHours, startdate)
+    return [(d, generateReceipt(ReceiptType.HOUR, d)) for d in dateList]
+
+
+def generateVerificationTable(receiptType, receiptList):
+    table = VerificationTable(receiptType)
+    for (datetimeObj, receipt) in receiptList:
+        sha = hashlib.sha256()
+        sha.update((str(randSaltGenerator())).encode())
+        sha.update(receipt.encode())
+        verifyHex = sha.hexdigest()
+        table.addCodeValuePair(verifyHex, datetimeObj)
+    return table
+
+
+def verifyReceipt(receipt, verificationTable):
     for saltValue in range(SALT_SIZE):
         sha = hashlib.sha256()
-        sha.update(saltValue, receipt)
-        potentialHash = sha.hexdigest()
-        if potentialHash in hashList[receiptType].keys():
-            return hashDict[receiptType][potentialHash]
-    return -1
+        sha.update((str(saltValue)).encode())
+        sha.update(receipt.encode())
+        potentialCode = sha.hexdigest()
+        value = verificationTable.getValue(potentialCode)
+        if (value != None):
+            return value
+    return None
 
 
 if __name__ == "__main__":
@@ -76,8 +148,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser_args = parser.parse_args()
 
-    print(getCurrentHour())
-    print(getCurrentDate())
-    print(randSaltGenerator())
-    print(randBytesGenerator(16))
-    print(generateReceipt(ReceiptType.HOUR, getCurrentHour(), getCurrentDate()))
+    # Testing
+    dates = generateDatetimeReceiptPairs(1)
+    table = generateVerificationTable(ReceiptType.HOUR, dates)
+
+    # Test
+    print(dates[0][1])
+    print(verifyReceipt(dates[0][1], table))
+
+
+
+
+
+
