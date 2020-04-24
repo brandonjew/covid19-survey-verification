d8e228d586cca3a0216ecec910fc0a1efdcb8eef
Brandon <brandon.jew@ucla.edu>
2020-04-23 21:27:52 -0700

    happy with webpage template

--- webpages/template.js
+++ webpages/template.js
@@ -1,4 +1,3 @@
-var s = document.getElementsByTagName('p');
 var date = new Date();
 var receipt = document.getElementById("receipt").textContent;
 var receiptDate = new Date(document.getElementById("keydate").textContent);
@@ -6,8 +5,43 @@ var qrcode = new QRCode("qrcode", {
     width: 300,
     height: 300,
 });
-
 qrcode.makeCode(receipt);
 
-console.log(receiptDate);
-console.log(date);
+var age = date - receiptDate;
+var squareColor = "grey-square";
+
+if (age < 0) {
+  // Comes from the future :O
+  squareColor = "grey-square";
+} else if (age < 8.64e7) {
+  // Less than a day old
+  squareColor = "blue-square";
+} else if (age < 6.048e8) {
+  // Less than a week old
+  squareColor = "yellow-square";
+} else {
+  // Older than a week
+  squareColor = "red-square";
+}
+
+document.getElementById("square").className = squareColor; 
+
+
+var options = {
+  year: 'numeric',
+  month: 'long',
+  day: '2-digit',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: false,
+  timeZoneName: 'short',
+};
+
+document.getElementById("keydate").textContent = new Intl.DateTimeFormat('default', options).format(receiptDate);
+document.getElementById("now").textContent = new Intl.DateTimeFormat('default', options).format(date);
+
+function changeTimeStyle() {
+  options.hour12 = !(options.hour12)
+  document.getElementById("keydate").textContent = new Intl.DateTimeFormat('default', options).format(receiptDate);
+  document.getElementById("now").textContent = new Intl.DateTimeFormat('default', options).format(date);
+}
